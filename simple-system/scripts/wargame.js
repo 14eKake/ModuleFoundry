86dfa775954920b8aa506c07c4d2e6a6c1ed527e
[14e]Kake <78047867+14eKake@users.noreply.github.com>
2025-07-03 10:56:05 +0200

    Add basic wargame actor types

---
+++ simple-system/scripts/wargame.js
@@ -0,0 +1,25 @@
+Hooks.once('init', () => {
+  console.log('Wargame system initializing');
+
+  class WargameActor extends Actor {
+    prepareData() {
+      super.prepareData();
+      const system = this.system;
+      system.move ??= 0;
+      system.attack ??= 0;
+      system.defense ??= 0;
+    }
+  }
+
+  CONFIG.Actor.documentClass = WargameActor;
+
+  class WargameActorSheet extends ActorSheet {
+    /** @override */
+    get template() {
+      return `systems/${game.system.id}/templates/actor-sheet.html`;
+    }
+  }
+
+  Actors.registerSheet('wargame', WargameActorSheet, { makeDefault: true });
+});
+
